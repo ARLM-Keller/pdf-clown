7ed4c186f46ed48d10453b55a0cba674788b246e
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2023-01-27 11:41:09 +0600

    Static initializer

--- dotNET/PdfClown/Tokens/Charset.cs
+++ dotNET/PdfClown/Tokens/Charset.cs
@@ -6,20 +6,18 @@ namespace PdfClown.Tokens
 {
     internal static class Charset
     {
+        static Charset()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
         public static readonly text::Encoding ISO88591 = text::Encoding.GetEncoding("ISO-8859-1");
         public static readonly text::Encoding UTF16BE = text::Encoding.BigEndianUnicode;
         public static readonly text::Encoding UTF16LE = text::Encoding.Unicode;
         public static readonly text::Encoding ASCII = text::Encoding.ASCII;
         public static readonly text::Encoding UTF8 = text::Encoding.UTF8;
-        private static bool registered;
 
         public static text::Encoding GetEnconding(string name)
         {
-            if (!registered)
-            {
-                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-                registered = true;
-            }
             return text::Encoding.GetEncoding(name);
         }
     }
